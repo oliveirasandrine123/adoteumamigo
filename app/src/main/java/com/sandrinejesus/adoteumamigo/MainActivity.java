e31df86701c6f5a6fc3c12316e7d237059b366bb
Sandrine Oliveira <sandrineoliveira9@gmail.com>
2016-11-18 21:54:01 -0200

    Config Firebase

--- app/src/main/java/com/sandrinejesus/adoteumamigo/MainActivity.java
+++ app/src/main/java/com/sandrinejesus/adoteumamigo/MainActivity.java
@@ -1,15 +1,28 @@
 package com.sandrinejesus.adoteumamigo;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
 
     private EditText user;
     private EditText password;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +32,35 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         user = (EditText) findViewById(R.id.user);
         password = (EditText) findViewById(R.id.password);
 
-        findViewById(R.id.login).setOnClickListener(this);
-        findViewById(R.id.cadastrar).setOnClickListener(this);
+        mAuth = FirebaseAuth.getInstance();
+
+        findViewById(R.id.login).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.signInWithEmailAndPassword(user.getText().toString(), password.getText().toString())
+                        .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    if (user != null ){
+                                        Intent it = new Intent(MainActivity.this, CadastroAnimal.class);
+                                        startActivity(it);
+                                        finish();
+                                    }
+                                } else {
+                                    Toast.makeText(MainActivity.this, "Usuario ou senha invalida", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+                }
+            });
+
+        findViewById(R.id.cadastrar).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cadastrarUsuario();
+            }
+        });
 
     }
 
@@ -29,11 +69,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         startActivity(i);
     }
 
-    @Override
-    public void onClick(View view) {
-        int i = view.getId();
-        if(i == R.id.cadastrar){
-            cadastrarUsuario();
-        }
-    }
 }
+
+
+
+
